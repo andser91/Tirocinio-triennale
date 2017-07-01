eaa1a5f7602c67f6949dbf9412f5b73451cf2f4a
ANDREA-PC\Andrea <andreaserrecchia@hotmail.it>
2017-07-01 14:33:28 +0200

    cambiati i grafi OO

--- user_tests/config.py
+++ user_tests/config.py
@@ -1,6 +1,6 @@
 
-TEST_DESCRIPTOR = 'static/test/test2/testDescriptor/test2.json'
-QUESTION_DIRECTORY = "static/test/test2/questions/"
+TEST_DESCRIPTOR = 'static/test/test1/testDescriptor/test1.json'
+QUESTION_DIRECTORY = "static/test/test1/questions/"
 JS_DIRECTORY = 'static/js/'
 TEST_LENGTH = 36
 RANGES_OF_RANDOMIZATION = [[0,8],[9,35]]

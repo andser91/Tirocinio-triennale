b28c15885e369f261fb000aa057083cb0fdd0843
ANDREA-PC\Andrea <andreaserrecchia@hotmail.it>
2017-07-12 14:42:34 +0200

    corrette piccole cose

--- user_tests/config.py
+++ user_tests/config.py
@@ -3,6 +3,6 @@ TEST_DESCRIPTOR = 'static/test/test1/testDescriptor/test1.json'
 QUESTION_DIRECTORY = 'static/test/test1/questions/'
 JS_DIRECTORY = 'static/js/'
 TEST_LENGTH = 27
-RANGES_OF_RANDOMIZATION = []
+RANGES_OF_RANDOMIZATION = [[0,8],[9,26]]
 
 
